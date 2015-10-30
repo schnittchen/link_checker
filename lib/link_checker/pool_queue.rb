4d14e8e1b8b97fa795c18d721cf4de3cb4b4f426
Thomas Stratmann <thomas.stratmann@9elements.com>
2015-10-30 19:23:38 +0100

    PoolQueue

---
+++ lib/link_checker/pool_queue.rb
@@ -0,0 +1,50 @@
+require 'thread'
+
+module LinkChecker
+
+class PoolQueue
+  def initialize(pool_size)
+    @pool_size = pool_size
+    @queue = Queue.new
+
+    @mtx = Mutex.new
+    @cond = ConditionVariable.new
+
+    @workers = (1..pool_size).map {
+      Thread.new(&method(:thread_body))
+    }
+  end
+
+  def push_job(callable = nil, &block)
+    callable ||= block
+    @queue << callable
+  end
+
+  def finished?
+    @queue.empty? && @queue.num_waiting == @pool_size
+  end
+
+  def wait_until_finished
+    @mtx.synchronize do
+      while !finished?
+        @cond.wait(@mtx)
+      end
+    end
+  end
+
+  private
+
+  def thread_body
+    while job = fetch_job
+      job.call
+    end
+  end
+
+  def fetch_job
+    @cond.signal if @queue.empty?
+    @queue.pop
+  end
+end
+
+end
+
