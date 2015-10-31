e81cc6442cd0f98261b1329827903f4051f7cf5d
Thomas Stratmann <thomas.stratmann@9elements.com>
2015-10-31 14:51:08 +0100

    many fixes to Uri

--- lib/link_checker/uri.rb
+++ lib/link_checker/uri.rb
@@ -17,6 +17,10 @@ class Uri
       false
     end
 
+    def same_host_and_scheme?(*)
+      raise ArgumentError
+    end
+
     def normalize
       self
     end
@@ -46,6 +50,13 @@ class Uri
     !!stdlib_uri
   end
 
+  def same_host_and_scheme?(uri)
+    raise ArgumentError unless valid?
+    raise ArgumentError unless uri.valid?
+    stdlib_uri.scheme == uri.stdlib_uri.scheme &&
+      stdlib_uri.host == uri.stdlib_uri.host
+  end
+
   def normalize
     if stdlib_uri = stdlib_uri()
       if stdlib_uri.query
@@ -60,33 +71,36 @@ class Uri
     end
   end
 
-  def to_absolute(from_uri) # from_uri may be nil if already absolute
-    if from_uri
-      from_uri = from_uri.stdlib_uri
-      raise ArgumentError unless from_uri # given from_uri was invalid
-      raise ArgumentError unless from_uri.absolute?
+  def to_absolute(from_uri)
+    return self unless valid?
+    return self if absolute?
+
+    raise ArgumentError unless from_uri.valid?
+    raise ArgumentError unless from_uri.absolute?
+
+    new_uri = from_uri.stdlib_uri.clone
+    path = Pathname(new_uri.path)
+    new_uri.path = path.join(Pathname(stdlib_uri.path)).to_s
+    self.class.new(new_uri.to_s)
+  end
+
+  def absolute?
+    raise ArgumentError unless valid?
+
+    if stdlib_uri.absolute?
+      binding.pry if stdlib_uri.path && !stdlib_uri.path.start_with?('/')
     end
 
-    if stdlib_uri = stdlib_uri()
-      if stdlib_uri.absolute? # has scheme
-        self # already absolute
-      else
-        raise ArgumentError unless from_uri
-
-        new_uri = from_uri.clone
-        path = Pathname(from_uri.path)
-        new_uri.path = path.join(Pathname(stdlib_uri.path)).to_s
-        self.class.new(new_uri.to_s)
-      end
-    else
-      self # equally invalid
-    end
+    # since stdlib uris have all been normalized, their path
+    # is either nil (in which case it is probably a URI::Generic which we cannot handle)
+    # or starts with "/". Hence, is suffices to check for presence of a scheme
+    stdlib_uri.absolute?
   end
 
   def stdlib_uri
     return @stdlib_uri if defined?(@stdlib_uri)
 
-    @stdlib_uri = URI(@str) rescue nil
+    @stdlib_uri = URI(@str).normalize rescue nil
   end
 end
 
